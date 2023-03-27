08a45b7d51f9759c988c98695b5fbcf264621324
mundi <christine.mundi@student.moringaschool.com$ git config --global user.email christine.mundi@student.moringaschool.com>
2023-03-27 21:56:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+    
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        const id = data.id;
+        document.body.innerHTML = id;
+      })
+      .catch(error => {
+        console.log(error.message);
+        document.body.innerHTML = error.message;
+      });
+  }
+  
